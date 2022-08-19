c296ad0a1a601df8262f3d293be950d499aa21db
RominM <rominmanogil.off@gmail.com>
2022-08-19 11:46:30 +0200

    feat: :construction: get url params in CarDetails

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,9 @@
     <div v-for="(car, Index) in cars" :key="Index">
       <router-link :to="`/car-details/${car.Name}`" class="car">  
         <li title="Click for more details">
-          {{car.Name}}
+          <p>
+            {{car.Name.charAt(0).toUpperCase() + car.Name.slice(1)}}
+          </p>
         </li>
       </router-link>
     </div>
@@ -28,16 +30,15 @@
   }
 
   li{
-    min-width: 300px;
-    max-width: 350px;
-    min-height: 100px;
+    display: table;
+    width: 320px;
+    height: 100px;
     margin: 25px auto;
     border: solid;
     border-radius: 10px;
     list-style: none;
     box-shadow: 3px 3px 10px;
     text-align: center;
-    line-height: 100px;
     cursor: pointer;
     transition: .1s;
   }
@@ -45,13 +46,23 @@
     transform: scale(1.07);
     background-color: hsla(160, 100%, 37%, 0.2);
   }
+  p{
+    text-align:center; 
+    vertical-align: middle;
+    display: table-cell;
+    max-width: 90%;
+    margin: auto;
+    font-size: x-large;
+  }
 
   @media (max-width: 780px) {
     ul {
       display: block;
+      padding: 0;
+      margin: 0;
     }
     li {
-      min-width: 215px;
+      width: 100%;
     }
   }
     @media (max-width: 400px) {
--- src/views/CarDetails.vue
+++ src/views/CarDetails.vue
@@ -3,12 +3,35 @@
     <h1>Car details</h1>
     <router-link to="/">Go back Home</router-link>
   </div>
+  <main>
+    <h2 class="name-car">{{ $route.params.id.toUpperCase() }}</h2>
+    <div>{{ car }}</div>
+  </main>
 </template>
 
 <script>
   export default {
     props: {
       cars: Object,
+    },
+    getCar() {
+      return cars.find(car => car.Name === this.url_data);
+    },
+    mounted(){
+      this.url_data=this.$route.params.id;
+    },
+    data(){
+      return{
+        url_data: null
+      };
     }
   };
 </script>
+
+<style>
+  .name-car{
+    text-align: center;
+    font-size: 3rem;
+    font-weight: bold;
+  }
+</style>
